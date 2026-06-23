19762aa897a905a1ff864905a64a10c28c335f57
SHIBAM-GHOSH <shibamghosh2004@gmail.com>
2026-06-23 22:23:50 +0530

    Add User entity and Role enum

---
+++ src/main/java/com/urbanfix/entity/User.java
@@ -0,0 +1,32 @@
+package com.urbanfix.entity;
+
+import com.urbanfix.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/urbanfix/enums/Role.java
@@ -0,0 +1,8 @@
+package com.urbanfix.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
+
+
