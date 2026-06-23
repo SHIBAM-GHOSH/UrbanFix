0f03c0434107712191dbfea5f2c6819520f11282
SHIBAM-GHOSH <shibamghosh2004@gmail.com>
2026-06-23 21:32:44 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/urbanfix/UrbanfixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urbanfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanfixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanfixBackendApplication.class, args);
+	}
+
+}
